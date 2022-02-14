69b9d11304caa1c822d84271b47c6afa5bcd2419
Valentin Féron <vferon@pentalog.com>
2022-02-14 12:43:35 -0600

    Removed plunk from dependencies

--- http2py/tests/conftest.py
+++ http2py/tests/conftest.py
@@ -1,11 +1,26 @@
 import pytest
-from plunk.tw.py2py_front_example.simple_pycode import foo, bar, confuser
 from py2http import run_app
 from py2http.util import run_process
 
 
+def foo(a: int = 0, b: int = 0, c=0):
+    """This is foo. It computes something"""
+    return (a * b) + c
+
+
+def bar(x, greeting="hello"):
+    """bar greets its input"""
+    return f"{greeting} {x}"
+
+
+def confuser(a: int = 0, x: float = 3.14):
+    return (a ** 2) * x
+
+
 @pytest.fixture(scope='session', autouse=True)
 def ws_app():
+
+
     with run_process(
         func=run_app,
         func_kwargs=dict(
