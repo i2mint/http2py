ae94374c7a7e85e640c88824fdc8a6d5f7aba81a
Thor Whalen <thorwhalen1@gmail.com>
2023-11-08 17:43:37 +0100

    feat: route extraction utils

---
+++ http2py/tests/util_test.py
@@ -0,0 +1,140 @@
+"""Test the util module."""
+
+import yaml
+import pytest
+from http2py.util import files, get_routes, Routes
+
+test_data_path = files / "tests" / "test_data"
+simple_openapi_spec_path = test_data_path / "simple_openapi_spec.yaml"
+
+
+def _openapi_spec():
+    return yaml.safe_load(simple_openapi_spec_path.read_text())
+
+
+@pytest.fixture
+def openapi_spec():
+    return _openapi_spec()
+
+
+def test_get_routes(openapi_spec):
+    result = list(get_routes(openapi_spec))
+    assert result == [('get', '/items'), ('post', '/items')]
+
+    result = list(get_routes(openapi_spec, include_methods='get'))
+    assert result == [('get', '/items')]
+
+    result = list(get_routes(openapi_spec, include_methods='post'))
+    assert result == [('post', '/items')]
+
+    result = list(get_routes(openapi_spec, include_methods='put'))
+    assert result == []
+
+
+def test_routes(openapi_spec):
+    routes = Routes(openapi_spec)
+    assert list(routes) == [('get', '/items'), ('post', '/items')]
+
+    get_route = routes['get', '/items']
+
+    assert get_route.input_specs == {
+        'parameters': [
+            {
+                'in': 'query',
+                'name': 'type',
+                'schema': {'type': 'string'},
+                'required': True,
+                'description': 'Type of items to list',
+            },
+            {
+                'in': 'query',
+                'name': 'limit',
+                'schema': {'type': 'integer', 'default': 10},
+                'required': False,
+                'description': 'Maximum number of items to return',
+            },
+        ],
+        'requestBody': {},
+    }
+
+    assert get_route.output_specs == {
+        '200': {
+            'description': 'An array of items',
+            'content': {
+                'application/json': {
+                    'schema': {
+                        'type': 'array',
+                        'items': {
+                            'type': 'object',
+                            'properties': {
+                                'id': {'type': 'integer'},
+                                'name': {'type': 'string'},
+                                'active': {'type': 'boolean'},
+                            },
+                        },
+                    }
+                }
+            },
+        }
+    }
+
+    assert get_route.parameters == [
+        {
+            'in': 'query',
+            'name': 'type',
+            'schema': {'type': 'string'},
+            'required': True,
+            'description': 'Type of items to list',
+        },
+        {
+            'in': 'query',
+            'name': 'limit',
+            'schema': {'type': 'integer', 'default': 10},
+            'required': False,
+            'description': 'Maximum number of items to return',
+        },
+    ]
+
+    post_route = routes['post', '/items']
+
+    assert post_route.input_specs == {
+        'parameters': [],
+        'requestBody': {
+            'required': True,
+            'content': {
+                'application/json': {
+                    'schema': {
+                        'type': 'object',
+                        'properties': {
+                            'name': {'type': 'string'},
+                            'age': {'type': 'integer', 'default': 42},
+                        },
+                    }
+                }
+            },
+        },
+    }
+
+    assert post_route.output_specs == {
+        '201': {
+            'description': 'Item created',
+            'content': {
+                'application/json': {
+                    'schema': {
+                        'type': 'object',
+                        'properties': {
+                            'id': {
+                                'type': 'integer',
+                                'description': 'Unique identifier of the created item',
+                            }
+                        },
+                    }
+                }
+            },
+        }
+    }
+
+    assert post_route.parameters == [
+        {'in': 'body', 'name': 'name', 'schema': {'type': 'string'}},
+        {'in': 'body', 'name': 'age', 'schema': {'type': 'integer', 'default': 42}},
+    ]
--- http2py/util.py
+++ http2py/util.py
@@ -1,5 +1,16 @@
 import json
 
+try:
+    import importlib.resources
+
+    _files = importlib.resources.files  # only valid in 3.9+
+except AttributeError:
+    import importlib_resources  # needs pip install
+
+    _files = importlib_resources.files
+
+files = _files("http2py")
+
 
 def add_defaults(d, dflts):
     """Adds defaults to every (mapping) value of every item of d"""
@@ -49,3 +60,120 @@ def is_jsonable(x):
         return True
     except (TypeError, OverflowError):
         return False
+
+
+# -------------------------------------------------------------------------------------
+# OpenAPI specs parser
+# Note: Perhaps a better way to do this is to use a "parsing" class with cache_property
+
+from typing import Any, Dict, Iterable, Iterator
+from functools import cached_property, partial
+from dol import KvReader, cached_keys
+from dataclasses import dataclass, field
+
+http_methods = {'get', 'post', 'put', 'delete', 'patch', 'options', 'head'}
+
+
+def get_routes(d: Dict[str, Any], include_methods=tuple(http_methods)) -> Iterable[str]:
+    """
+    Takes OpenAPI specification dict 'd' and returns the key-paths to all the endpoints.
+    """
+    if isinstance(include_methods, str):
+        include_methods = {include_methods}
+    for endpoint in (paths := d.get('paths', {})):
+        for method in paths[endpoint]:
+            if method in include_methods:
+                yield method, endpoint
+
+
+dflt_type_mapping = tuple(
+    {
+        "array": list,
+        "integer": int,
+        "object": dict,
+        "string": str,
+        "boolean": bool,
+        "number": float,
+    }.items()
+)
+
+
+@cached_keys
+class Routes(KvReader):
+    def __init__(self, spec: dict, *, type_mapping: dict = dflt_type_mapping) -> None:
+        self.spec = spec
+        self._mk_route = partial(Route, spec=spec, type_mapping=type_mapping)
+        self._title = spec.get('info', {}).get('title', 'OpenAPI spec')
+
+    @classmethod
+    def from_yaml(cls, yaml_str: str):
+        import yaml
+
+        return cls(yaml.safe_load(yaml_str))
+
+    @property
+    def _paths(self):
+        self.spec['paths']
+
+    def __iter__(self):
+        return get_routes(self.spec)
+
+    def __getitem__(self, k):
+        return self._mk_route(*k)
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}('{self._title}')"
+
+
+@dataclass
+class Route:
+    method: str
+    endpoint: str
+    spec: dict = field(repr=False)
+    # TODO: When moving to 3.9+, make below keyword-only
+    type_mapping: dict = field(default=dflt_type_mapping, repr=False)
+
+    def __post_init__(self):
+        self.type_mapping = dict(self.type_mapping)
+
+    @cached_property
+    def method_data(self):
+        method, endpoint = self.method, self.endpoint
+        method_data = self.spec.get('paths', {}).get(endpoint, {}).get(method, None)
+        if method_data is None:
+            raise KeyError(f"Endpoint '{endpoint}' has no method '{method}'")
+        return method_data
+
+    @cached_property
+    def input_specs(self):
+        return {
+            'parameters': self.method_data.get('parameters', []),
+            'requestBody': self.method_data.get('requestBody', {}),
+        }
+
+    @cached_property
+    def output_specs(self):
+        return self.method_data.get('responses', {})
+
+    @cached_property
+    def parameters(self):
+        # Start with the parameters defined in the 'parameters' section
+        params = self.method_data.get('parameters', [])
+
+        # Check if requestBody is defined and has content with a JSON content type
+        request_body = self.method_data.get('requestBody', {})
+        content = request_body.get('content', {}).get('application/json', {})
+
+        # If there's a schema, we extract its properties and merge with the parameters
+        if 'schema' in content:
+            schema_props = content['schema'].get('properties', {})
+            for name, details in schema_props.items():
+                params.append(
+                    {
+                        'in': 'body',  # or 'in': 'requestBody', if that makes more sense in your context
+                        'name': name,
+                        'schema': details,
+                    }
+                )
+
+        return params
